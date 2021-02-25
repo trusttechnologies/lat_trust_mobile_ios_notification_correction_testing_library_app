ca90a4050bcea3c3696c00febc98b904c59b4517
Kevin Torres Montenegro <kevin.johan.yerel@gmail.com>
2021-02-25 10:51:39 -0300

    git ignore added

---
+++ NotiCorrectionLib/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  NotiCorrectionLib
+//
+//  Created by Kevin Torres on 25-02-21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder {}
+
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
